732a1a18685559dfb71303db6df6d37680b72abc
razvan.ungurasu <razvan.ungurasu@raisissoftware.com>
2020-11-21 17:27:39 +0200

    Initial commit

---
+++ android/app/src/main/java/com/raisissoftware/COURSES/MainActivity.java
@@ -0,0 +1,6 @@
+package com.raisissoftware.COURSES;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'src/01/01.dart' as app;
+
+
+    void main(){
+      app.main();
+    }
